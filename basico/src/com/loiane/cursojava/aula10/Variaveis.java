22c76c2800a4edc651c94c76dda4ee679268186a
joao-cmr <contatojoaocmr@gmail.com>
2025-10-04 18:54:55 -0300

    Variaveis

---
+++ basico/src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade= 35;
+		String nome= "Joao Carlos";
+	
+		
+		System.out.println("Meu nome é " + nome);
+		System.out.println("Minha idade é: " + idade);
+	}
+
+}
